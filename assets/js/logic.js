602ffde49a7642d474dcd2eb95b21bf1a2bf4580
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-01-13 19:48:17 +0000

    add pseudocode for script

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+
+// When I press the start button, the welcome page is hidden, 
+// the first question and potential answers to said question become visible 
+// + the countdown starts (from 60 seconds)
+
+
+// If the user gets the question wrong, time is deducted from the countdown (10 seconds)
+
+// When the user completes all questions, the remaining time becomes their score
+// If the user reaches 0 seconds, it is the end of the quiz (they score a 0)
+
+// Save the score to localStorage (the scores will be displayed from highest to lowest) - allow the user to clear high score 
+// data with a click of a button 
+
+
