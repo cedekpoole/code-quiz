d53cfb3e3c0f71535873efb1bc7b2450b4ea361d
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-01-13 19:47:42 +0000

    add quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,97 @@
+const questions = [
+  {
+    question:
+      "What method can we use to stop elements from functioning in the default way?",
+    options: [
+      "stopDefault()",
+      "stopPropagation()",
+      "preventDefault()",
+      "removeChild()",
+    ],
+    answerIndex: 3,
+  },
+
+  {
+    question:
+      "Which built-in method removes the last element from an array and returns that element?",
+    options: [
+      "last()",
+      "pop()",
+      "push()",
+      "remove()",
+    ],
+    answerIndex: 2,
+  },
+
+  {
+    question:
+      "Which one of the following is the correct way for calling the JavaScript code?",
+    options: [
+      "Preprocessor",
+      "Triggering Event",
+      "RMI",
+      "Function/Method",
+    ],
+    answerIndex: 4,
+  },
+
+  {
+    question:
+      "In the JavaScript, which one of the following is not considered as an error:",
+    options: [
+      "Missing a semicolon",
+      "Syntax error",
+      "Division of zero",
+      "Missing a bracket",
+    ],
+    answerIndex: 1,
+  },
+
+  {
+    question:
+      "Choose the correct snippet from the following to check if the variable 'a' is not equal the 'NULL':",
+    options: [
+      "if(a!==null",
+      "if (a!)",
+      "if(a!null)",
+      "if(a!=null)",
+    ],
+    answerIndex: 1,
+  },
+
+  {
+    question:
+      "Which one of the following operator is used to check weather a specific property exists or not:",
+    options: [
+      "Exists",
+      "within",
+      "found",
+      "in",
+    ],
+    answerIndex: 4,
+  },
+
+  {
+    question:
+      "Which one of the following is an ternary operator:",
+    options: [
+      "+",
+      "||",
+      "?",
+      ":",
+    ],
+    answerIndex: 3,
+  },
+
+  {
+    question:
+      "What we will get if we compare the 'one' with '8' using the less than operator ('one'< 8)?",
+    options: [
+      "True",
+      "False",
+      "Undefined",
+      "NaN",
+    ],
+    answerIndex: 2,
+  },
+];
