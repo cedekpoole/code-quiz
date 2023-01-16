5dad3164f9b96cb0a2035abe61cc0d47e1f56710
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-01-16 14:12:47 +0000

    add event listeners to the buttons

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,51 +3,76 @@ var startScreen = document.querySelector("#start-screen");
 var timeEl = document.querySelector("#time");
 var questionContainerEl = document.querySelector("#questions");
 var questionEl = document.querySelector("#question-title");
-var choicesEl = document.querySelector("#choices")
+var choicesEl = document.querySelector("#choices");
+var endScreenEl = document.querySelector("#end-screen");
+var finalScoreEl = document.querySelector("#final-score");
 
 // create button elements for the multiple choice options
 var btn1 = document.createElement("button");
 var btn2 = document.createElement("button");
 var btn3 = document.createElement("button");
 var btn4 = document.createElement("button");
+var rightOrWrong = document.createElement("p");
+choicesEl.appendChild(btn1);
+choicesEl.appendChild(btn2);
+choicesEl.appendChild(btn3);
+choicesEl.appendChild(btn4);
+questionContainerEl.appendChild(rightOrWrong);
 
+var timeLeft = questions.length * 10;
 // create a countdown function
-function countdown() {
-  timeLeft = questions.length * 10;
+let countdown = () => {
   var timeInterval = setInterval(function () {
     timeLeft--;
     timeEl.textContent = timeLeft;
-    if (timeLeft === 0) {
+    if (timeLeft <= 0 || runningQ === questions.length) {
       clearInterval(timeInterval);
       timeEl.textContent = "";
+      questionContainerEl.style.display = "none";
+      endScreenEl.style.display = "block";
+      finalScoreEl.textContent = timeLeft;
     }
   }, 1000);
 }
-
-startBtn.addEventListener("click", function () {
-  startScreen.setAttribute("style", "display: none");
+// When the start button is clicked, hide the start screen display and render the first question
+startBtn.addEventListener("click", () => {
+  startScreen.style.display = "none";
+  questionContainerEl.style.display = "block";
+  runningQ = 0;
   renderQ();
+  countdown();
 });
 
+// Create a variable that picks a specific question from the array of questions
 var runningQ = 0;
-var lastQ = questions.length - 1;
+var q = questions[runningQ];
 
-
-function renderQ() {
-  var q = questions[runningQ];
-  questionContainerEl.setAttribute("style", "display: block");
+var renderQ = () => {
   questionEl.textContent = q.question;
-  choicesEl.appendChild(btn1);
-  choicesEl.appendChild(btn2);
-  choicesEl.appendChild(btn3);
-  choicesEl.appendChild(btn4);
   btn1.textContent = q.options[0];
   btn2.textContent = q.options[1];
   btn3.textContent = q.options[2];
   btn4.textContent = q.options[3];
-  runningQ++;
-  countdown();
 }
+
+var checkOption = (e) => {
+    e.preventDefault();
+    // display the 'right' or 'wrong' p element for a limited amount of time
+    setTimeout(function() {
+        rightOrWrong.style.display = "none";
+    }, 1200);
+    if (questions[runningQ].answer === e.target.value) {
+        rightOrWrong.textContent = "Correct Answer!"
+    } else {
+        rightOrWrong.textContent = "Wrong Answer!"
+        timeLeft -= 10;
+    }
+}
+
+choicesEl.addEventListener("click", checkOption)
+
+
+// create an event listener for when the right answer is clicked 
 
 
 // When I press the start button, the welcome page is hidden,
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,95 +3,95 @@ const questions = [
     question:
       "What method can we use to stop elements from functioning in the default way?",
     options: [
-      "stopDefault()",
-      "stopPropagation()",
-      "preventDefault()",
-      "removeChild()",
+      "1. stopDefault()",
+      "2. stopPropagation()",
+      "3. preventDefault()",
+      "4. removeChild()",
     ],
-    answerIndex: 3,
+    answerIndex: "3. preventDefault()",
   },
 
   {
     question:
       "Which built-in method removes the last element from an array and returns that element?",
     options: [
-      "last()",
-      "pop()",
-      "push()",
-      "remove()",
+      "1. last()",
+      "2. pop()",
+      "3. push()",
+      "4. remove()",
     ],
-    answerIndex: 2,
+    answerIndex: "2. pop()",
   },
 
   {
     question:
       "Which one of the following is the correct way for calling the JavaScript code?",
     options: [
-      "Preprocessor",
-      "Triggering Event",
-      "RMI",
-      "Function/Method",
+      "1. Preprocessor",
+      "2. Triggering Event",
+      "3. RMI",
+      "4. Function/Method",
     ],
-    answerIndex: 4,
+    answerIndex: "4. Function/Method",
   },
 
   {
     question:
       "In the JavaScript, which one of the following is not considered as an error:",
     options: [
-      "Missing a semicolon",
-      "Syntax error",
-      "Division of zero",
-      "Missing a bracket",
+      "1. Missing a semicolon",
+      "2. Syntax error",
+      "3. Division of zero",
+      "4. Missing a bracket",
     ],
-    answerIndex: 1,
+    answerIndex: "1. Missing a semicolon",
   },
 
   {
     question:
       "Choose the correct snippet from the following to check if the variable 'a' is not equal the 'NULL':",
     options: [
-      "if(a!==null",
-      "if (a!)",
-      "if(a!null)",
-      "if(a!=null)",
+      "1. if(a!==null)",
+      "2. if (a!)",
+      "3. if(a!null)",
+      "4. if(a!=null)",
     ],
-    answerIndex: 1,
+    answerIndex: "1. if(a!==null)",
   },
 
   {
     question:
       "Which one of the following operator is used to check weather a specific property exists or not:",
     options: [
-      "Exists",
-      "within",
-      "found",
-      "in",
+      "1. Exists",
+      "2. within",
+      "3. found",
+      "4. in",
     ],
-    answerIndex: 4,
+    answerIndex: "4. in",
   },
 
   {
     question:
       "Which one of the following is an ternary operator:",
     options: [
-      "+",
-      "||",
-      "?",
-      ":",
+      "1. +",
+      "2. ||",
+      "3. ?",
+      "4. :",
     ],
-    answerIndex: 3,
+    answerIndex: "3. ?",
   },
 
   {
     question:
       "What we will get if we compare the 'one' with '8' using the less than operator ('one'< 8)?",
     options: [
-      "True",
-      "False",
-      "Undefined",
-      "NaN",
+      "1. True",
+      "2. False",
+      "3. Undefined",
+      "4. NaN",
     ],
-    answerIndex: 2,
+    answerIndex: "2. False",
   },
 ];
