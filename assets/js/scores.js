fd86c4d57175ff9b855c4ecf93c292ec8e0f98fb
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-01-19 12:14:36 +0000

    refactor showHighscores() function

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -4,7 +4,6 @@
 var highscoreList = document.querySelector("#highscores");
 var clearBtn = document.querySelector("#clear");
 
-
 // When clear highscore button is pressed, it clears the local storage and refreshes the page
 clearBtn.addEventListener("click", () => {
   window.localStorage.removeItem("highscore");
@@ -12,20 +11,20 @@ clearBtn.addEventListener("click", () => {
 });
 
 function showHighScores() {
-    // if there are values in the highscore key, place them in a variable
-    // otherwise, set the variable to an empty array
-    var scoreObjects = JSON.parse(window.localStorage.getItem("highscore")) || [];
-    // sort the scores from highest to lowest
-    scoreObjects.sort((num1, num2) => {
-        return num2.hScore - num1.hScore;
-    })
-    // for each user (their score + initials) in the local storage, append their details onto the high score list
-    scoreObjects.forEach(points => {
-        var scoreLi = document.createElement("li");
-        scoreLi.textContent = `${points.initials}: ${points.hScore}`;
-        highscoreList.appendChild(scoreLi);
-        scoreLi.style.backgroundColor = "black";
-    })
+  // if there are values in the highscore key, place them in a variable
+  // otherwise, set the variable to an empty array
+  var scoreObjects = JSON.parse(window.localStorage.getItem("highscore")) || [];
+  // sort the scores from highest to lowest
+  scoreObjects.sort((num1, num2) => {
+    return num2.hScore - num1.hScore;
+  });
+  // for each user (their score + initials) in the local storage, append their details onto the high score list
+  for (let scores of scoreObjects) {
+    var scoreLi = document.createElement("li");
+    scoreLi.textContent = `${scores.initials}: ${scores.hScore}`;
+    highscoreList.appendChild(scoreLi);
+    scoreLi.style.backgroundColor = "black";
+  }
 }
 
 //execute the function when the page loads (allow highscores to show)
